5042f262d749d2b5cc1505dc82982675ae3c247e
ashbhandare <ash.bhandare@gmail.com>
2016-01-29 17:09:43 +0530

    clean hand on depth

--- kinect_body_ash.py
+++ kinect_body_ash.py
@@ -31,55 +31,7 @@ class HandGestureObjectClass(object):
         self._bodies = None
 
 
-    def draw_body_bone(self, joints, jointPoints, color, joint0, joint1,print_frame):
-        joint0State = joints[joint0].TrackingState;
-        joint1State = joints[joint1].TrackingState;
-
-        # both joints are not tracked
-        if (joint0State == PyKinectV2.TrackingState_NotTracked) or (joint1State == PyKinectV2.TrackingState_NotTracked): 
-            return
-
-        # both joints are not *really* tracked
-        if (joint0State == PyKinectV2.TrackingState_Inferred) and (joint1State == PyKinectV2.TrackingState_Inferred):
-            return
-
-        # ok, at least one is good 
-        start = (jointPoints[joint0].x, jointPoints[joint0].y)
-        end = (jointPoints[joint1].x, jointPoints[joint1].y)
-
-        try:
-            print ':drawing bone'
-
-            print_frame = cv2.line(print_frame, start, end, color, 8)
-            
-        except: # need to catch it due to possible invalid positions (with inf)
-            pass
-
-    def draw_body(self, joints, jointPoints, color,print_frame):
-
-
-        rx=jointPoints[PyKinectV2.JointType_HandRight].x
-        ry=jointPoints[PyKinectV2.JointType_HandRight].y
-        lx=jointPoints[PyKinectV2.JointType_HandLeft].x
-        ly=jointPoints[PyKinectV2.JointType_HandLeft].y
-
-        print_frame=cv2.circle(print_frame,(rx,ry), 100,(255,0,0),1)
-        print_frame=cv2.circle(print_frame,(lx,ly), 100,(255,0,0),1)
-        # # Right Arm    
-        # self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_ShoulderRight, PyKinectV2.JointType_ElbowRight,print_frame);
-        # self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_ElbowRight, PyKinectV2.JointType_WristRight,print_frame);
-        # self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_WristRight, PyKinectV2.JointType_HandRight,print_frame);
-        # self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_HandRight, PyKinectV2.JointType_HandTipRight,print_frame);
-        # self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_WristRight, PyKinectV2.JointType_ThumbRight,print_frame);
-
-        # # Left Arm
-        # self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_ShoulderLeft, PyKinectV2.JointType_ElbowLeft,print_frame);
-        # self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_ElbowLeft, PyKinectV2.JointType_WristLeft,print_frame);
-        # self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_WristLeft, PyKinectV2.JointType_HandLeft,print_frame);
-        # self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_HandLeft, PyKinectV2.JointType_HandTipLeft,print_frame);
-        # self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_WristLeft, PyKinectV2.JointType_ThumbLeft,print_frame);
-
-       
+    
     
     def run(self):
         print_frame=None
@@ -126,7 +78,7 @@ class HandGestureObjectClass(object):
 
                         print_frame=cv2.circle(print_frame,(int(rx),int(ry)), 10,(255,0,0),5)
                         print_frame=cv2.circle(print_frame,(int(lx),int(ly)), 10,(255,0,0),5)
-                        #self.draw_body(joints, joint_points, SKELETON_COLORS[i],print_frame)
+
 
                 if print_frame != None:
 
